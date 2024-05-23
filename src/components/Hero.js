40bc54453841194880e2fcb88055ff42d0e511f8
Surnmai <surnmai1@gmail.com>
2024-05-23 21:22:22 +0000

    Hero changed

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,7 +3,7 @@ import React from "react";
 const Hero = () => {
   return (
     <>
-      <section className="bg-hero3 h-[100vh] w-full bg-cover bg-no-repeat bg-center brightness-[40%]">
+      <section className="bg-hero2 h-[100vh] w-full bg-cover bg-no-repeat bg-center brightness-[40%]">
         <div className="container mx-auto flex justify-center items-center">
           <h3 className="md:text-8xl text-4xl text-[#ff2eb2] font-bold z-50 translate-y-96 tracking-tight uppercase">
             Ellakeh
